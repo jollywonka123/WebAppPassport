4743a19b8b2adf8a4b47d59c08d34c6a107166fa
victor <viktordobra578@icloud.com>
2026-04-19 15:13:48 +0300

    fix: in repos was added processing with null-values

--- WebAppPassport/DataBase/Repositories/CountryUserRepository.cs
+++ WebAppPassport/DataBase/Repositories/CountryUserRepository.cs
@@ -9,32 +9,42 @@ public class CountryUserRepository(AppContext context): BaseRepository(context),
     public async Task<ICollection<(Country, User)>> GetAllAsync()
     {
         var users = await Context.Users.Include(x => x.Countries).ToListAsync();
-
-        return users
-            .SelectMany(
-                user => user.Countries,
-                (user, country) => (country, user)
-            )
-            .ToList();
+        if (users.Any())
+        {
+            return users
+                .SelectMany(
+                    user => user.Countries!,
+                    (user, country) => (country, user)
+                )
+                .ToList();
+        }
+        return new List<(Country, User)>();
     }
 
     public async Task AddAsync((Country, User) entity)
     {
-        entity.Item1.Users.Add(entity.Item2);
-        entity.Item2.Countries.Add(entity.Item1);
-        await Context.SaveChangesAsync();
+        if (entity.Item1.Users != null && entity.Item2.Countries != null)
+        {
+            entity.Item1.Users.Add(entity.Item2);
+            entity.Item2.Countries.Add(entity.Item1);
+            await Context.SaveChangesAsync();
+        }
     }
 
     public async Task<ICollection<(Country, User)>> GetByConditionAsync(Expression<Func<(Country, User), bool>> predicate)
     {
         var users = await Context.Users.Include(x => x.Countries).ToListAsync();
-        var func = predicate.Compile();
-        return users
-            .SelectMany(
-                user => user.Countries,
-                (user, country) => (country, user)
-            )
-            .Where(func)
-            .ToList();
+        if (users.Any())
+        {
+            var func = predicate.Compile();
+            return users
+                .SelectMany(
+                    user => user.Countries!,
+                    (user, country) => (country, user)
+                )
+                .Where(func)
+                .ToList();
+        }
+        return new List<(Country, User)>();
     }
 }
--- WebAppPassport/DataBase/Repositories/PassportUserRepository.cs
+++ WebAppPassport/DataBase/Repositories/PassportUserRepository.cs
@@ -9,32 +9,43 @@ public class PassportUserRepository(AppContext context): BaseRepository(context)
     public async Task<ICollection<(Passport, User)>> GetAllAsync()
     {
         var users = await Context.Users.Include(x => x.Passports).ToListAsync();
-
-        return users
-            .SelectMany(
-                user => user.Passports,
-                (user, passport) => (passport, user)
-            )
-            .ToList();
+        if (users.Any())
+        {
+            return users
+                .SelectMany(
+                    user => user.Passports!,
+                    (user, passport) => (passport, user)
+                )
+                .ToList();
+        }
+        return new List<(Passport, User)>();
+        
     }
 
     public async Task AddAsync((Passport, User) entity)
     {
-        entity.Item1.Users.Add(entity.Item2);
-        entity.Item2.Passports.Add(entity.Item1);
-        await Context.SaveChangesAsync();
+        if (entity.Item1.Users != null && entity.Item2.Passports != null)
+        {
+            entity.Item1.Users.Add(entity.Item2);
+            entity.Item2.Passports.Add(entity.Item1);
+            await Context.SaveChangesAsync();
+        }
     }
 
     public async Task<ICollection<(Passport, User)>> GetByConditionAsync(Expression<Func<(Passport, User), bool>> predicate)
     {
         var users = await Context.Users.Include(x => x.Passports).ToListAsync();
-        var func = predicate.Compile();
-        return users
-            .SelectMany(
-                user => user.Passports,
-                (user, passport) => (passport, user)
-            )
-            .Where(func)
-            .ToList();
+        if (users.Any())
+        {
+            var func = predicate.Compile();
+            return users
+                .SelectMany(
+                    user => user.Passports!,
+                    (user, passport) => (passport, user)
+                )
+                .Where(func)
+                .ToList();
+        }
+        return new List<(Passport, User)>();
     }
 }
